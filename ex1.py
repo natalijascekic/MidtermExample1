e9713b87d60342231f0eede2010bebe8e3c41f26
natalijascekic <natalija.scekic@udg.edu.me>
2018-03-08 11:13:31 +0100

    prvi zadatak

---
+++ ex1.py
@@ -0,0 +1,6 @@
+#konvertovati km u milje
+kilometri = input("Unesite kilometre:")
+kilomtri_float = float (kilometri)    #posto input vrati string pretvaramo u float (decimalni)
+milje = kilomtri_float * 0.6213       #1 km ima 0.6213 milja
+
+print(milje)
